5aa24a88132337433924268abbc245ae00dc07c0
Studsk <kvejborgrasmus@gmail.com>
2022-09-15 11:26:38 +0200

    Nyt commit med ints i og t

--- src/app/HelloWorld.java
+++ src/app/HelloWorld.java
@@ -5,5 +5,9 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Hello World again");
         System.out.println("Daniel");
+
+        int i = 2;
+        int t = 3;
+        System.out.println(i+t);
     }
 }
